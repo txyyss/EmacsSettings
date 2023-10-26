7f678a5f4647ab642d6e946e5bcf8090cb3006c6
Shengyi Wang <txyyss@gmail.com>
2023-10-25 22:37:35 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; 快捷键提醒
+;; 快捷键提醒
 ;; C-c @ C-q 展示大纲模式，隐藏其它
 ;; C-c @ C-s 显示子节点
 
@@ -7,8 +7,8 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 ;;; 设置环境变量
-(setenv "PATH" (concat "/opt/homebrew/bin:/opt/homebrew/sbin:~/.local/bin:/usr/local/bin:/Library/TeX/texbin:/Applications/Racket v8.7/bin:" (getenv "PATH")))
-(setq exec-path (append exec-path '("/opt/homebrew/bin" "/opt/homebrew/sbin" "~/.local/bin" "/usr/local/bin" "/Library/TeX/texbin" "/Applications/Racket v8.7/bin")))
+(setenv "PATH" (concat "/opt/homebrew/bin:/opt/homebrew/sbin:~/.local/bin:/usr/local/bin:/Library/TeX/texbin:/Applications/Racket v8.10/bin:" (getenv "PATH")))
+(setq exec-path (append exec-path '("/opt/homebrew/bin" "/opt/homebrew/sbin" "~/.local/bin" "/usr/local/bin" "/Library/TeX/texbin" "/Applications/Racket v8.10/bin")))
 (setenv "LC_ALL" "en_US.UTF-8")
 
 ;;; 设置语言
@@ -17,9 +17,10 @@
 (setq system-time-locale "C")
 
 ;;; 设置字体
-(set-face-font 'default "Sarasa Mono SC-18")
-;; Sarasa-Gothic Version = 0.37.4
-;; Download from https://github.com/be5invis/Sarasa-Gothic/releases
+(set-face-attribute 'default nil :family "Iosevka" :height 180)
+(set-fontset-font t 'han (font-spec :name "Source Han Sans"))
+;; Iosevka Version = 27.3.1
+;; Download from https://github.com/be5invis/Iosevka/releases
 
 ;;; 常用设置
 (add-hook 'dired-mode-hook 'treemacs-icons-dired-mode)
@@ -54,6 +55,10 @@
 
 (which-key-mode)
 
+(require 'ls-lisp)
+(setq ls-lisp-use-insert-directory-program nil)
+
+
 ;;; Org Mode 设置
 (define-key global-map [?\C-c ?a] 'org-agenda)
 (define-key global-map [?\C-c ?r] 'org-remember)
@@ -68,15 +73,139 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff"
+    "#eeeeec"])
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(company-backends '(company-math-symbols-unicode))
  '(company-idle-delay 0.3)
  '(company-minimum-prefix-length 2)
+ '(connection-local-criteria-alist
+   '(((:application tramp :protocol "kubernetes")
+      tramp-kubernetes-connection-local-default-profile)
+     ((:application tramp :protocol "flatpak")
+      tramp-container-connection-local-default-flatpak-profile
+      tramp-flatpak-connection-local-default-profile)
+     ((:application tramp :machine "localhost")
+      tramp-connection-local-darwin-ps-profile)
+     ((:application tramp :machine "ShengyideMacBook-Pro.local")
+      tramp-connection-local-darwin-ps-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile
+      tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-flatpak-connection-local-default-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin"
+                         "/usr/bin" "/sbin" "/usr/sbin"
+                         "/usr/local/bin" "/usr/local/sbin"
+                         "/local/bin" "/local/freeware/bin"
+                         "/local/gnu/bin" "/usr/freeware/bin"
+                         "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin"
+                         "/opt/sbin" "/opt/local/bin"))
+     (tramp-kubernetes-connection-local-default-profile
+      (tramp-config-check . tramp-kubernetes--current-context-data)
+      (tramp-extra-expand-args 97
+                               (tramp-kubernetes--container
+                                (car tramp-current-connection))
+                               104
+                               (tramp-kubernetes--pod
+                                (car tramp-current-connection))
+                               120
+                               (tramp-kubernetes--context-namespace
+                                (car tramp-current-connection))))
+     (tramp-container-connection-local-default-flatpak-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin"
+                         "/usr/bin" "/sbin" "/usr/sbin"
+                         "/usr/local/bin" "/usr/local/sbin"
+                         "/local/bin" "/local/freeware/bin"
+                         "/local/gnu/bin" "/usr/freeware/bin"
+                         "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin"
+                         "/opt/sbin" "/opt/local/bin"))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o"
+                                        "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o" "state=abcde" "-o"
+                                        "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (euid . number)
+                                          (user . string)
+                                          (egid . number) (comm . 52)
+                                          (state . 5) (ppid . number)
+                                          (pgrp . number)
+                                          (sess . number)
+                                          (ttname . string)
+                                          (tpgid . number)
+                                          (minflt . number)
+                                          (majflt . number)
+                                          (time . tramp-ps-time)
+                                          (pri . number)
+                                          (nice . number)
+                                          (vsize . number)
+                                          (rss . number)
+                                          (etime . tramp-ps-time)
+                                          (pcpu . number)
+                                          (pmem . number) (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o"
+                                        "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o" "stat=abcde" "-o"
+                                        "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (user . string)
+                                          (group . string) (comm . 52)
+                                          (state . 5) (ppid . number)
+                                          (pgrp . number)
+                                          (ttname . string)
+                                          (time . tramp-ps-time)
+                                          (nice . number)
+                                          (etime . tramp-ps-time)
+                                          (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o"
+                                        "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o"
+                                        "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (euid . number)
+                                          (user . string)
+                                          (egid . number)
+                                          (group . string) (comm . 52)
+                                          (state . string)
+                                          (ppid . number)
+                                          (pgrp . number)
+                                          (sess . number)
+                                          (ttname . string)
+                                          (tpgid . number)
+                                          (minflt . number)
+                                          (majflt . number)
+                                          (time . tramp-ps-time)
+                                          (pri . number)
+                                          (nice . number)
+                                          (vsize . number)
+                                          (rss . number)
+                                          (etime . number)
+                                          (pcpu . number)
+                                          (pmem . number) (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh") (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":") (null-device . "/dev/null"))))
  '(custom-enabled-themes '(tron-legacy))
  '(custom-safe-themes
-   '("e80b1078c4ce2225f6f8d7621a55d3b675c86cad505b22b20243d4d075f491f5" "f0f5bfda176875f70299b2a3a07e778f23b8af81defe3bc40babd0a85f88d411" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "c44380b9483c0808694bc2a721ca626cc1925f51f580dcefbc0fc072f1906111" "a5b0e366b1bf62133ae7c303a3b36b443a13aff40bf2bc0319143aba2caa4a87" "a21dcd2f5b8c38e799b9dcb91f702d5d864263a794732aa3ac05c73de070d1da" "6af9a4651ed0662d64b5ee15d74ce56d0421a3101caaf513687b74d7c42853f2" default))
+   '("7fd9e3ef1d713ffbf413ee6abf9cb0e6f7106cba6dea7c3e89fe1b3449cf99bd"
+     "c87e2c5c3b8a4a0375e2cd73dcb9b8eb56e8f8da465d2bdad252d96de08764b5"
+     "c5bfb24f5c6fbb099dcf13a51cb99804126a069bad6e18d8ed99e3bb536f72f2"
+     "da161909c8b030d429176176392acc28e9e69f046e55f4919ba9840d15179840"
+     "46821b856b14ed0c6fe6e11e6abeb5f8a575c2fd5ed4bcc33fbe52b7487ac20e"
+     "0d09f49e81c811f4a661ff63b82d2b812ce034aed336903922b06362b8cb0bbe"
+     "e80b1078c4ce2225f6f8d7621a55d3b675c86cad505b22b20243d4d075f491f5"
+     "f0f5bfda176875f70299b2a3a07e778f23b8af81defe3bc40babd0a85f88d411"
+     "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa"
+     "c44380b9483c0808694bc2a721ca626cc1925f51f580dcefbc0fc072f1906111"
+     "a5b0e366b1bf62133ae7c303a3b36b443a13aff40bf2bc0319143aba2caa4a87"
+     "a21dcd2f5b8c38e799b9dcb91f702d5d864263a794732aa3ac05c73de070d1da"
+     "6af9a4651ed0662d64b5ee15d74ce56d0421a3101caaf513687b74d7c42853f2"
+     default))
  '(delete-selection-mode t)
  '(dired-recursive-copies 'always)
  '(dired-recursive-deletes 'always)
@@ -84,9 +213,6 @@
  '(display-time-day-and-date t)
  '(display-time-default-load-average nil)
  '(display-time-mode t)
- '(doom-modeline-buffer-encoding 'nondefault)
- '(doom-modeline-height 0)
- '(doom-modeline-mode t)
  '(gap-executable "/usr/local/bin/gap")
  '(gap-start-options '("-f" "-b" "-m" "2m" "-E"))
  '(geiser-chez-binary "chez")
@@ -101,7 +227,13 @@
  '(ns-command-modifier 'meta)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(opam-switch-mode proof-general org-variable-pitch geiser-chez doom-modeline company-coq xbm-life geiser-chicken treemacs-icons-dired treemacs-all-the-icons treemacs winum rustic diminish which-key lsp-ui flycheck tuareg tron-legacy-theme geiser-racket geiser-guile glsl-mode geiser paredit gap-mode elpy))
+   '(async-status company company-coq diminish f flycheck gap-mode geiser
+                  geiser-chez geiser-guile geiser-racket glsl-mode
+                  ligature lsp-mode magit-section mood-line
+                  opam-switch-mode org-variable-pitch paredit
+                  proof-general treemacs treemacs-all-the-icons
+                  treemacs-icons-dired tron-legacy-theme tuareg
+                  which-key xbm-life))
  '(python-shell-interpreter "/usr/local/bin/python3")
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
@@ -126,19 +258,16 @@
 ;; (setq sokoban-levels-dir "c:/Useful/Open/emacs/site-lisp/sokoban-levels")
 
 ;; Python
-(elpy-enable)
-(setq elpy-rpc-python-command "python3")
+;; (elpy-enable)
+;; (setq elpy-rpc-python-command "python3")
 
 ;;; ParEdit
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-
-;;; Rust
-(add-hook 'rustic-mode-hook 'yas-minor-mode)
+;; (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+;; (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+;; (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+;; (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;; (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
 ;;; 杂项
 (defun zh-count-word ()
@@ -166,7 +295,9 @@
 (require 'diminish)
 (diminish 'which-key-mode)
 (diminish 'paredit-mode)
-(tuareg-opam-update-env (tuareg-opam-current-compiler))
+
+(require 'tuareg)
+(opam-switch-set-switch (tuareg-opam-current-compiler))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -178,7 +309,49 @@
  '(org-done ((t nil)))
  '(variable-pitch ((t (:family "Libertinus Serif")))))
 
+(defun f2c (fahrenheit)
+  (interactive "nFahrenheit (°F): ")
+  (message "%s °C" (/ (* 5 (- fahrenheit 32)) 9.0)))
 
+(defun c2f (celsius)
+  (interactive "nCelsius (°C): ")
+  (message "%s °F" (+ (/ (* celsius 9.0) 5.0) 32)))
+
+(mood-line-mode)
+(setq mood-line-glyph-alist mood-line-glyphs-unicode)
+
+
+;; Lean 4 mode
+(setq load-path (cons "/Users/shengyiwang/Program/Lisp/lean4-mode" load-path))
+
+(setq lean4-mode-required-packages '(dash f flycheck lsp-mode magit-section s))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(let ((need-to-refresh t))
+  (dolist (p lean4-mode-required-packages)
+    (when (not (package-installed-p p))
+      (when need-to-refresh
+        (package-refresh-contents)
+        (setq need-to-refresh nil))
+      (package-install p))))
+
+(require 'lean4-mode)
+
+(defvar ligatures-iosevka
+  '("-<<" "-<" "-<-" "<--" "<---" "<<-" "<-" "->" "->>" "--->" "-->" "->-" ">-" ">>-"
+    "=<<" "=<" "=<=" "<==" "<===" "<<=" "<=" "=>" "=>>" "===>" "==>" "=>=" ">=" ">>="
+    "<->" "<-->" "<--->" "<---->" "<=>" "<==>" "<===>" "<====>" "::" ":::" "__"
+    "<~~" "</" "</>" "/>" "~~>" "==" "!=" "<>" "===" "!==" "!==="
+    "<:" ":=" "*=" "*+" "<*" "<*>" "*>" "<|" "<|>" "|>" "<." "<.>" ".>" "+*" "=*" "=:" ":>"
+    "(*" "*)" "/*" "*/" "[|" "|]" "{|" "|}" "++" "+++" "\\/" "/\\" "|-" "-|" "<!--" "<!---"))
+
+(use-package ligature
+  :config
+  (ligature-set-ligatures 'prog-mode ligatures-iosevka)
+  (ligature-set-ligatures 'coq-mode ligatures-iosevka)
+  (global-ligature-mode t))
 
 ;; Local Variables:
 ;; mode: outline-minor;
