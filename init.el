9da8a718f985d61a174122c341e09a7d3a672373
Shengyi Wang <txyyss@gmail.com>
2024-12-19 02:22:33 -0500

    feat: remove tramp customs, add completion settings and some others.

--- init.el
+++ init.el
@@ -5,8 +5,8 @@
 ;; C-c @ C-s 显示子节点
 
 ;;; 设置环境变量
-;; (setenv "PATH" (concat "/opt/homebrew/bin:/opt/homebrew/sbin:~/.local/bin:/usr/local/bin:/Library/TeX/texbin:/Applications/Racket v8.14/bin:" (getenv "PATH")))
-;; (setq exec-path (append exec-path '("/opt/homebrew/bin" "/opt/homebrew/sbin" "~/.local/bin" "/usr/local/bin" "/Library/TeX/texbin" "/Applications/Racket v8.14/bin")))
+(setenv "PATH" (concat "/opt/homebrew/bin:/opt/homebrew/sbin:" (getenv "PATH")))
+(setq exec-path (append exec-path '("/opt/homebrew/bin" "/opt/homebrew/sbin")))
 (setenv "LC_ALL" "en_US.UTF-8")
 
 ;;; 设置语言
@@ -143,145 +143,12 @@
  '(company-idle-delay 0.3)
  '(company-minimum-prefix-length 2)
  '(completion-category-overrides '((file (styles basic partial-completion))))
+ '(completion-cycle-threshold 3)
  '(completion-pcm-leading-wildcard t)
  '(completion-styles '(orderless basic))
  '(confirm-kill-processes nil)
- '(connection-local-criteria-alist
-   '(((:application vc-git) vc-git-connection-default-profile)
-     ((:application tramp :machine "ShengyiMacBook-Pro.local")
-      tramp-connection-local-darwin-ps-profile)
-     ((:application tramp :machine
-                    "dynamic-oit-ip4-wifirestricted01-10-16-217-168.princeton.edu")
-      tramp-connection-local-darwin-ps-profile)
-     ((:application eshell) eshell-connection-default-profile)
-     ((:application tramp :protocol "kubernetes")
-      tramp-kubernetes-connection-local-default-profile)
-     ((:application tramp :protocol "flatpak")
-      tramp-container-connection-local-default-flatpak-profile
-      tramp-flatpak-connection-local-default-profile)
-     ((:application tramp :machine "localhost")
-      tramp-connection-local-darwin-ps-profile)
-     ((:application tramp :machine "ShengyideMacBook-Pro.local")
-      tramp-connection-local-darwin-ps-profile)
-     ((:application tramp)
-      tramp-connection-local-default-system-profile
-      tramp-connection-local-default-shell-profile)))
- '(connection-local-profile-alist
-   '((vc-git-connection-default-profile (vc-git--program-version))
-     (eshell-connection-default-profile (eshell-path-env-list))
-     (tramp-flatpak-connection-local-default-profile
-      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin"
-                         "/usr/bin" "/sbin" "/usr/sbin"
-                         "/usr/local/bin" "/usr/local/sbin"
-                         "/local/bin" "/local/freeware/bin"
-                         "/local/gnu/bin" "/usr/freeware/bin"
-                         "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin"
-                         "/opt/sbin" "/opt/local/bin"))
-     (tramp-kubernetes-connection-local-default-profile
-      (tramp-config-check . tramp-kubernetes--current-context-data)
-      (tramp-extra-expand-args 97
-                               (tramp-kubernetes--container
-                                (car tramp-current-connection))
-                               104
-                               (tramp-kubernetes--pod
-                                (car tramp-current-connection))
-                               120
-                               (tramp-kubernetes--context-namespace
-                                (car tramp-current-connection))))
-     (tramp-container-connection-local-default-flatpak-profile
-      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin"
-                         "/usr/bin" "/sbin" "/usr/sbin"
-                         "/usr/local/bin" "/usr/local/sbin"
-                         "/local/bin" "/local/freeware/bin"
-                         "/local/gnu/bin" "/usr/freeware/bin"
-                         "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin"
-                         "/opt/sbin" "/opt/local/bin"))
-     (tramp-connection-local-darwin-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o"
-                                        "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o" "state=abcde" "-o"
-                                        "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (euid . number)
-                                          (user . string)
-                                          (egid . number) (comm . 52)
-                                          (state . 5) (ppid . number)
-                                          (pgrp . number)
-                                          (sess . number)
-                                          (ttname . string)
-                                          (tpgid . number)
-                                          (minflt . number)
-                                          (majflt . number)
-                                          (time . tramp-ps-time)
-                                          (pri . number)
-                                          (nice . number)
-                                          (vsize . number)
-                                          (rss . number)
-                                          (etime . tramp-ps-time)
-                                          (pcpu . number)
-                                          (pmem . number) (args)))
-     (tramp-connection-local-busybox-ps-profile
-      (tramp-process-attributes-ps-args "-o"
-                                        "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o" "stat=abcde" "-o"
-                                        "ppid,pgid,tty,time,nice,etime,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (user . string)
-                                          (group . string) (comm . 52)
-                                          (state . 5) (ppid . number)
-                                          (pgrp . number)
-                                          (ttname . string)
-                                          (time . tramp-ps-time)
-                                          (nice . number)
-                                          (etime . tramp-ps-time)
-                                          (args)))
-     (tramp-connection-local-bsd-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o"
-                                        "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                        "-o"
-                                        "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format (pid . number)
-                                          (euid . number)
-                                          (user . string)
-                                          (egid . number)
-                                          (group . string) (comm . 52)
-                                          (state . string)
-                                          (ppid . number)
-                                          (pgrp . number)
-                                          (sess . number)
-                                          (ttname . string)
-                                          (tpgid . number)
-                                          (minflt . number)
-                                          (majflt . number)
-                                          (time . tramp-ps-time)
-                                          (pri . number)
-                                          (nice . number)
-                                          (vsize . number)
-                                          (rss . number)
-                                          (etime . number)
-                                          (pcpu . number)
-                                          (pmem . number) (args)))
-     (tramp-connection-local-default-shell-profile
-      (shell-file-name . "/bin/sh") (shell-command-switch . "-c"))
-     (tramp-connection-local-default-system-profile
-      (path-separator . ":") (null-device . "/dev/null"))))
  '(consult-preview-excluded-files exclude-file-list)
  '(custom-enabled-themes '(modus-vivendi))
- '(custom-safe-themes
-   '("7fd9e3ef1d713ffbf413ee6abf9cb0e6f7106cba6dea7c3e89fe1b3449cf99bd"
-     "c87e2c5c3b8a4a0375e2cd73dcb9b8eb56e8f8da465d2bdad252d96de08764b5"
-     "c5bfb24f5c6fbb099dcf13a51cb99804126a069bad6e18d8ed99e3bb536f72f2"
-     "da161909c8b030d429176176392acc28e9e69f046e55f4919ba9840d15179840"
-     "46821b856b14ed0c6fe6e11e6abeb5f8a575c2fd5ed4bcc33fbe52b7487ac20e"
-     "0d09f49e81c811f4a661ff63b82d2b812ce034aed336903922b06362b8cb0bbe"
-     "e80b1078c4ce2225f6f8d7621a55d3b675c86cad505b22b20243d4d075f491f5"
-     "f0f5bfda176875f70299b2a3a07e778f23b8af81defe3bc40babd0a85f88d411"
-     "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa"
-     "c44380b9483c0808694bc2a721ca626cc1925f51f580dcefbc0fc072f1906111"
-     "a5b0e366b1bf62133ae7c303a3b36b443a13aff40bf2bc0319143aba2caa4a87"
-     "a21dcd2f5b8c38e799b9dcb91f702d5d864263a794732aa3ac05c73de070d1da"
-     "6af9a4651ed0662d64b5ee15d74ce56d0421a3101caaf513687b74d7c42853f2"
-     default))
  '(delete-old-versions t)
  '(delete-selection-mode t)
  '(dired-dwim-target 'dired-dwim-target-next)
@@ -331,7 +198,7 @@
    '((border-mode-line-active unspecified)
      (border-mode-line-inactive unspecified) (bg-tab-bar bg-main)
      (bg-tab-current bg-active) (bg-tab-other bg-dim)
-     (fringe unspecified) ,@modus-themes-preset-overrides-faint))
+     (fringe unspecified)))
  '(mood-line-glyph-alist
    '((:checker-info . 8627) (:checker-issues . 9873)
      (:checker-good . 10004) (:checker-checking . 10227)
@@ -347,6 +214,7 @@
  '(ns-command-modifier 'meta)
  '(orderless-matching-styles '(orderless-regexp orderless-literal orderless-prefixes))
  '(org-appear-autoentities t)
+ '(org-appear-autosubmarkers t)
  '(org-babel-load-languages '((emacs-lisp . t) (lisp . t)))
  '(org-confirm-babel-evaluate nil)
  '(org-hide-emphasis-markers t)
@@ -363,7 +231,7 @@
                   lean4-mode ligature magit marginalia mood-line
                   opam-switch-mode orderless org-appear org-modern
                   osx-dictionary paredit pdf-tools proof-general slime
-                  tuareg vertico xbm-life yaml-mode))
+                  tuareg vertico yaml-mode))
  '(package-vc-selected-packages
    '((lean4-mode :url
                  "https://github.com/leanprover-community/lean4-mode.git")))
@@ -376,6 +244,7 @@
  '(scheme-mode-hook '(geiser-mode--maybe-activate enable-paredit-mode) t)
  '(scroll-bar-mode nil)
  '(switch-to-buffer-obey-display-actions t)
+ '(tab-always-indent 'complete)
  '(tab-bar-auto-width-max '(423 40))
  '(tab-bar-close-button-show nil)
  '(tab-bar-format '(tab-bar-format-tabs))
